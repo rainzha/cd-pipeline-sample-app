e9c16ee1d054c31c77e76250ad3e55e3e15bdd3a
rainzha <rainzha@163.com>
2019-07-19 17:52:59 +0800

    init project.

---
+++ src/main/java/org/rainzha/cdpipelinesampleapp/CdPipelineSampleAppApplication.java
@@ -0,0 +1,13 @@
+package org.rainzha.cdpipelinesampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdPipelineSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdPipelineSampleAppApplication.class, args);
+	}
+
+}
